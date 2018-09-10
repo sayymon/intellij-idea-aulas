ace3c18a8f39863f842fcbff0361a45b868ad741
sayymon <sayymon@hotmail.com>
2018-09-09 21:36:49 -0300

    Commite das aulas de Intellij Atualização

--- src/br/com/alura/bytebank/Principal.java
+++ src/br/com/alura/bytebank/Principal.java
@@ -5,6 +5,7 @@ import br.com.alura.bytebank.visualizador.OpcoesPagamento;
 public class Principal {
     public static void main(String[] args) {
         new OpcoesPagamento().mensagemDeBoasVindas();
+
     }
 
 }
